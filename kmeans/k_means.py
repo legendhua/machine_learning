a2862a4c5871de5277484bccdbe6079fb7feba3d
legendhua <zhangguanghualgd@163.com>
2017-07-09 23:14:54 +0800

    add kmeans

---
+++ kmeans/k_means.py
@@ -0,0 +1,89 @@
+
+import numpy as np
+from numpy import *
+import matplotlib.pyplot as plt
+
+
+# calculate Euclidean distance
+def euclDistance(vector1, vector2):
+    return sqrt(sum(power(vector2 - vector1, 2)))
+
+
+# init centroids with random samples
+def initCentroids(dataSet, k):
+    numSamples, dim = dataSet.shape
+    centroids = zeros((k, dim))
+    for i in range(k):
+        index = int(random.uniform(0, numSamples))
+        centroids[i, :] = dataSet[index, :]
+    return centroids
+
+
+# k-means cluster
+def kmeans(dataSet, k):
+    numSamples = dataSet.shape[0]
+    # 第一列存储该数据属于哪一类
+    # 第二列存储该数据和其中心的距离
+    clusterAssment = mat(zeros((numSamples, 2)))
+    clusterChanged = True  #定义聚类中心是否改变
+
+    ## step 1: init centroids
+    centroids = initCentroids(dataSet, k)
+
+    while clusterChanged:
+        clusterChanged = False
+        ## for each sample
+        for i in range(numSamples):
+            minDist = 100000.0
+            minIndex = 0
+            ## for each centroid
+            ## step 2: find the centroid who is closest
+            for j in range(k):
+                distance = euclDistance(centroids[j, :], dataSet[i, :])
+                if distance < minDist:
+                    minDist = distance
+                    minIndex = j
+
+            ## step 3: update its cluster
+            if clusterAssment[i, 0] != minIndex:
+                clusterChanged = True
+                clusterAssment[i, :] = minIndex, minDist ** 2
+
+        ## step 4: update centroids
+        for j in range(k):
+            pointsInCluster = dataSet[nonzero(clusterAssment[:, 0].A == j)[0]]
+            centroids[j, :] = mean(pointsInCluster, axis=0)
+
+    print('Congratulations, cluster complete!')
+    return centroids, clusterAssment
+
+
+# show your cluster only available with 2-D data
+def showCluster(dataSet, k, centroids, clusterAssment):
+    numSamples, dim = dataSet.shape
+    if dim != 2:
+        print("Sorry! I can not draw because the dimension of your data is not 2!")
+        return 1
+
+    mark = ['or', 'ob', 'og', 'ok', '^r', '+r', 'sr', 'dr', '<r', 'pr']
+    if k > len(mark):
+        print("Sorry! Your k is too large! ")
+        return 1
+
+    # draw all samples
+    for i in range(numSamples):
+        markIndex = int(clusterAssment[i, 0])
+        plt.plot(dataSet[i, 0], dataSet[i, 1], mark[markIndex])
+
+    mark = ['Dr', 'Db', 'Dg', 'Dk', '^b', '+b', 'sb', 'db', '<b', 'pb']
+    # draw the centroids
+    for i in range(k):
+        plt.plot(centroids[i, 0], centroids[i, 1], mark[i], markersize=12)
+
+    plt.show()
+
+if __name__=="__main__":
+    data = np.array([[1,2],[3,6],[6,6],[7,12],[5,8],[23,3],[4,3],[12,14],[12,18],[12,14]])
+    k = 3
+    centroids, clusterAssment = kmeans(data, k)
+    showCluster(data, k, centroids, clusterAssment)
